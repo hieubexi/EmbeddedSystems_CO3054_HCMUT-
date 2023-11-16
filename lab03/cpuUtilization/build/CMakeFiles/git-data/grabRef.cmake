ac0253d5da0b6eea7df1e4d4127648c013bb2dd4
Hieu Nguyen <hieu.nguyen301202@gmail.com>
2023-11-16 13:35:41 +0700

    add Extra

--- lab03/cpuUtilization/build/CMakeFiles/git-data/grabRef.cmake
+++ lab03/cpuUtilization/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Semester_231/Embeded_System/Lab/lab03/cpuUtilization/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "D:/Semester_231/Embeded_System/Lab/.git")
+set(GIT_DIR "C:/Users/hieun/esp/esp-idf/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- lab03/cpuUtilization/main/main.c
+++ lab03/cpuUtilization/main/main.c
@@ -1,22 +1,27 @@
-// #include "esp32_perform.h"
-#include "esp32_perfmon.h"
+/*
+* creator/date: HieuHuuNguyen_2013149/Nov.16.2023
+* course name/ID: Embedded Systems/CO3054
+*/
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_freertos_hooks.h"
 #include "sdkconfig.h"
-
+#include "esp_err.h"
 #include "esp_log.h"
-static const char *TAG = "perform";
+static const char *TAG = "CPU utilization:";
 
 static uint64_t idle0Calls = 0;
 static uint64_t idle1Calls = 0;
-
+// MaxIdleCalls represents the maximum number of idle calls (total time)
 #if defined(CONFIG_ESP32_DEFAULT_CPU_FREQ_240)
 static const uint64_t MaxIdleCalls = 1855000;
+// static const uint64_t MaxIdleCalls = 2000000;
 #elif defined(CONFIG_ESP32_DEFAULT_CPU_FREQ_160)
 static const uint64_t MaxIdleCalls = 1233100;
+// static const uint64_t MaxIdleCalls = 2000000;
 #else
-#error "Unsupported CPU frequency"
+#error "Unsupported CPU frequency" 
 #endif
 
 static bool idle_task_0()
@@ -40,23 +45,22 @@ static void perform_task(void *args)
 		idle0Calls = 0;
 		idle1Calls = 0;
 
-		int cpu0 = 100.f -  idle0 / MaxIdleCalls * 100.f;
-		int cpu1 = 100.f - idle1 / MaxIdleCalls * 100.f;
+		int utilize_cpu0 = 100.f -  idle0 / MaxIdleCalls * 100.f;
+		int utilize_cpu1 = 100.f - idle1 / MaxIdleCalls * 100.f;
 
-		ESP_LOGI(TAG, "Core 0 at %d%%", cpu0);
-		ESP_LOGI(TAG, "Core 1 at %d%%", cpu1);
+		ESP_LOGI(TAG, "Core 0 at %d%%", utilize_cpu0);
+		ESP_LOGI(TAG, "Core 1 at %d%%", utilize_cpu1);
 		// TODO configurable delay
-		vTaskDelay(5000 / portTICK_PERIOD_MS);
+		vTaskDelay(1200 / portTICK_PERIOD_MS);
 	}
 	vTaskDelete(NULL);
 }
 
-esp_err_t perfmon_start()
-{
-	ESP_ERROR_CHECK(esp_register_freertos_idle_hook_for_cpu(idle_task_0, 0));
+
+void app_main(void) {
+   	ESP_ERROR_CHECK(esp_register_freertos_idle_hook_for_cpu(idle_task_0, 0));
 	ESP_ERROR_CHECK(esp_register_freertos_idle_hook_for_cpu(idle_task_1, 1));
-	// TODO calculate optimal stack size
+
 
 	xTaskCreate(perform_task, "perform", 2048, NULL, 1, NULL);
-	return ESP_OK;
 }
